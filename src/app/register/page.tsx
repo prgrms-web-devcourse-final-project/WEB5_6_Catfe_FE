89580395c26e63fffb521cb839531df646f42701
JinKeyp <wlsrkd7312@gmail.com>
2025-09-24 00:16:48 +0900

    피드백사항 적용

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -43,7 +43,7 @@ function RegisterPage() {
         <div className="w-full max-w-[600px]">
           <Image
             src="/catmodum/catmakase_02.png"
-            alt="고양카세"
+            alt="고양모둠"
             width={600}
             height={600}
             className="w-full h-auto object-contain"
@@ -51,30 +51,38 @@ function RegisterPage() {
           />
         </div>
         <div className="w-full max-w-[403px] flex flex-col gap-6">
-          <h1 className="text-[48px] md:text-[64px] font-semibold text-secondary-900 mb-5 text-center md:text-left">
+          <h2 className="text-[48px] md:text-[64px] font-semibold text-secondary-900 mb-5 text-center md:text-left">
             Register
-          </h1>
+          </h2>
           <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             {/* 아이디 */}
             <div className="relative">
+              <label htmlFor="register-id" className="sr-only">
+                아이디
+              </label>
               <input
                 id="register-id"
                 type="text"
                 placeholder="ID"
                 value={id}
                 onChange={(e) => setId(e.target.value)}
+                required
                 className="w-full h-[64px] pl-10 pr-4 border border-black rounded-md"
               />
               <Image
                 src="/icon/idIcon.svg"
-                alt="아이디"
+                alt=""
                 width={20}
                 height={20}
                 className="absolute left-3 top-1/2 -translate-y-1/2"
+                aria-hidden="true"
               />
             </div>
             {/* 비밀번호 */}
             <div className="relative">
+              <label htmlFor="register-password" className="sr-only">
+                비밀번호
+              </label>
               <input
                 id="register-password"
                 type={showPassword ? "text" : "password"}
@@ -82,10 +90,11 @@ function RegisterPage() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="w-full h-[64px] pl-10 pr-10 border border-black rounded-md"
+                required
               />
               <Image
                 src="/icon/pwIcon.svg"
-                alt="비밀번호"
+                alt=""
                 width={20}
                 height={20}
                 className="absolute left-3 top-1/2 -translate-y-1/2"
@@ -101,11 +110,15 @@ function RegisterPage() {
                   alt=""
                   width={20}
                   height={20}
+                  aria-hidden="true"
                 />
               </button>
             </div>
             {/* 비밀번호 확인 */}
             <div className="relative">
+              <label htmlFor="register-password-confirm" className="sr-only">
+                비밀번호 확인
+              </label>
               <input
                 id="register-password-confirm"
                 type={showPasswordConfirm ? "text" : "password"}
@@ -113,13 +126,15 @@ function RegisterPage() {
                 value={passwordConfirm}
                 onChange={(e) => setPasswordConfirm(e.target.value)}
                 className="w-full h-[64px] pl-10 pr-10 border border-black rounded-md"
+                required
               />
               <Image
                 src="/icon/pwIcon.svg"
-                alt="비밀번호 확인"
+                alt=""
                 width={20}
                 height={20}
                 className="absolute left-3 top-1/2 -translate-y-1/2"
+                aria-hidden="true"
               />
               <button
                 type="button"
@@ -132,11 +147,15 @@ function RegisterPage() {
                   alt=""
                   width={20}
                   height={20}
+                  aria-hidden="true"
                 />
               </button>
             </div>
             {/* 이름 */}
             <div className="relative">
+              <label htmlFor="register-name" className="sr-only">
+                이름
+              </label>
               <input
                 id="register-name"
                 type="text"
@@ -144,6 +163,7 @@ function RegisterPage() {
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 className="w-full h-[64px] pl-10 pr-4 border border-black rounded-md"
+                required
               />
               <Image
                 src="/icon/profile.svg"
@@ -151,14 +171,19 @@ function RegisterPage() {
                 width={20}
                 height={20}
                 className="absolute left-3 top-1/2 -translate-y-1/2"
+                aria-hidden="true"
               />
             </div>
             {/* 이메일 */}
             <div className="relative">
+              <label htmlFor="register-email" className="sr-only">
+                이메일
+              </label>
               <input
                 id="register-email"
                 type="email"
                 placeholder="Email"
+                required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 className="w-full h-[64px] pl-10 pr-4 border border-black rounded-md"
@@ -169,6 +194,7 @@ function RegisterPage() {
                 width={20}
                 height={20}
                 className="absolute left-3 top-1/2 -translate-y-1/2"
+                aria-hidden="true"
               />
             </div>
             <Button

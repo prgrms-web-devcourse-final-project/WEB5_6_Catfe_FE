fb482bcb2f6c027a11197e4fe31b89150d5a5014
EUNJIN <eafiseemn@gmail.com>
2025-09-24 16:18:01 +0900

    feat : 페이지 이동 시 ScrollTo: top 옵션 추가

--- src/app/community/[id]/ContentsDetail.tsx
+++ src/app/community/[id]/ContentsDetail.tsx
@@ -4,10 +4,15 @@ import CommentList from '@/components/community/CommentList';
 import CommunityContents from '@/components/community/CommunityContents';
 import Spinner from '@/components/Spinner';
 import { useComments, usePost } from '@/hook/useCommunityPost';
+import { useEffect } from 'react';
 
 function ContentsDetail({ postId }: { postId: string }) {
   const { data: post, isLoading: loadingPost } = usePost(postId);
   const { data: comments, isLoading: loadingComments } = useComments(postId);
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'instant' });
+  }, [postId]);
 
   // const loadingPost = true;
   // const loadingComments = true;

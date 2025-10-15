9109b3d600430c2fa65499de65d75cc73f6f2fa6
EUNJIN <eafiseemn@gmail.com>
2025-10-15 22:30:56 +0900

    fix : Search input & icon 사이 gap 없앰

--- src/components/study-rooms-list/SearchBar.tsx
+++ src/components/study-rooms-list/SearchBar.tsx
@@ -1,20 +1,24 @@
-import Image from "next/image";
-import React from "react";
+import Image from 'next/image';
+import React from 'react';
 
 type Props = {
   search?: string;
-  sort?: "all" | "public" | "enter" | "popular";
+  sort?: 'all' | 'public' | 'enter' | 'popular';
 };
 
-export default function SearchBar({ search = "", sort = "all" }: Props) {
+export default function SearchBar({ search = '', sort = 'all' }: Props) {
   return (
-    <form method="get" className="mx-auto mt-10 mb-20 flex max-w-lg justify-center items-center gap-2">
+    <form
+      method="get"
+      className="mx-auto mt-10 mb-20 flex max-w-lg justify-center items-center gap-2"
+    >
       <div className="w-full h-auto max-w-[480px] px-4 py-2 flex justify-between rounded-2xl border-2 border-text-secondary focus-within:border-secondary-600">
         <input
           name="search"
+          type="search"
           defaultValue={search}
           placeholder="스터디룸 검색"
-          className="text-sm outline-none px-2"
+          className="text-sm outline-none px-2 flex-1"
         />
         <button className="p-2 hover:bg-text-secondary/20 cursor-pointer rounded-xl">
           <Image src="/icon/study-room/search.svg" alt="검색 아이콘" width={20} height={20} />

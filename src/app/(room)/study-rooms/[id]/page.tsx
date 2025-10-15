58b5114a1b59eb8aeaa58c9f9e4f7aa089b011f5
cheezstick <yooha922@gmail.com>
2025-10-16 00:16:12 +0900

    fix : GuestHome 오류, 초대모달 오류, 스터디룸 동적 메타데이터 추가

--- src/app/(room)/study-rooms/[id]/page.tsx
+++ src/app/(room)/study-rooms/[id]/page.tsx
@@ -1,5 +1,10 @@
-
+import { Metadata } from "next";
 import RoomContent from "./RoomContent";
+
+export const metadata: Metadata = {
+  title: 'Catfé | StudyRoom ',
+  description: '스터디룸에 입장하셨습니다! 열공하세요😺',
+};
 
 export default function StudyRoomPage() {
 
--- src/components/study-room/InviteShareModal.tsx
+++ src/components/study-room/InviteShareModal.tsx
@@ -21,7 +21,7 @@ function InviteShareModal({ inviteCode, onClose }: Props) {
     const lines = [
       "Catfé 스터디룸에 초대합니다 🙌",
       `초대 코드: ${inviteCode}`,
-      `초대 코드 입력하러 가기 : https://catfe.com/study-rooms`,
+      `초대 코드 입력하러 가기 : ${process.env.NEXT_PUBLIC_API_URL}/study-rooms`,
     ];
     return lines.join("\n");
   }, [inviteCode]);
